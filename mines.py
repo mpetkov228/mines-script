d67b6e353eac9e13030a6927d533d2d242f58e8f
MihailPetkov96 <mpetkov228@gmail.com>
2025-01-25 20:55:04 +0200

    Add win condition

--- mines.py
+++ mines.py
@@ -1,7 +1,15 @@
 floating_wager = float(input("Enter starting wager: "))
 minimum_wager = floating_wager
+
 total = 0
 while True:
     result = input("1 for win | 0 for loss | end")
     if result == "end":
         break
+    
+    result = int(result)
+    if result == 1:
+        total += floating_wager * 1.47 - floating_wager
+        floating_wager = minimum_wager
+    
+    
